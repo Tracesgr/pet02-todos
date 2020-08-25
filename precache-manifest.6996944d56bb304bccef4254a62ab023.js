b34d16516411d8115e24dfca64f3d5af81cf524c
Raven <huuthang01061998@gmail.com>
2020-08-25 17:03:02 +0700

    Updates

---
+++ precache-manifest.6996944d56bb304bccef4254a62ab023.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01403bafa5ced1601421604586fe728b",
+    "url": "/pet02-todos/index.html"
+  },
+  {
+    "revision": "131fa893e802ab94601a",
+    "url": "/pet02-todos/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf042763f1ee8b5ff07f",
+    "url": "/pet02-todos/static/js/2.95ecd329.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/pet02-todos/static/js/2.95ecd329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "131fa893e802ab94601a",
+    "url": "/pet02-todos/static/js/main.536959d5.chunk.js"
+  },
+  {
+    "revision": "98cf93ba0a7f4e4ca65e",
+    "url": "/pet02-todos/static/js/runtime-main.a01b4c1c.js"
+  }
+]);
